03f99cb679ffaaa26a561ea292ca80ac468d9fa1
Gabriel Laufer <gabriellaufer13@gmail.com>
2024-06-08 10:21:05 -0300

    initial commit

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicial from "../pages/Inicial";
+import Sobre from "../pages/Sobre";
+import Body from "../layout/Body";
+import PrincipiosCard from "../pages/principios";
+import Theme from "../pages/theme";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter> {/* responsável por englobar o app em navegação */}
+      <Routes> {/* responsável pelas rotas */}
+        <Route path="/" element={<Body />}>
+          <Route path="/" element={<Inicial />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/principios" element={<PrincipiosCard />} />
+          <Route path="/info" element={<Theme />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
